b77fb024f6937d531488f5535024a0b374286cb7
Sahil <hanss9545@gmail.com>
2022-10-28 12:15:26 +0530

    menu responsive

--- nav.js
+++ nav.js
@@ -1,5 +1,10 @@
 const menuBar = document.querySelector(".mobile-menuBar");
 const menuBtn = document.querySelector(".menuBtn");
+const closeMenuBtn = document.querySelector(".close-menu");
 menuBtn.addEventListener("click", () => {
   menuBar.classList.add("show-menu");
+});
+
+closeMenuBtn.addEventListener("click", () => {
+  menuBar.classList.remove("show-menu");
 });
